8ddbaf14d538a501989097d38f232a54c0db875b
ozwind <chewi0@hotmail.com>
2023-05-13 15:48:59 -0700

    Add files via upload

--- memories.js
+++ memories.js
@@ -2,6 +2,7 @@ let index = 0;
 var images;
 var intervalId;
 const mcStore = 'memoriesCollection';
+var map;
 
 function init() {
     $("#startShow").on("click", startShow);
@@ -10,24 +11,26 @@ function init() {
     $("#previous").on("click", previous);
     $("#next").on("click", next);
     $("#reload").on("click", reload);
-    $(window).resize(setImageSize);
+    $(window).resize(resize);
+
+    $('#cbThumbs').change(function () {
+        showThumbs();
+    });
 
     $collection = $('#collection');
     let memCol = localStorage.getItem(mcStore);
     if ('family' === memCol) {
         images = family;
-    }
-    else if ('scenic' === memCol) {
+    } else if ('scenic' === memCol) {
         images = scenic;
-    }
-    else {
+    } else {
         images = family;
         memCol = 'family';
     }
 
     $('#collection').val(memCol);
 
-    $('#image').on('click', function() {
+    $('#image').on('click', function () {
         full();
     });
 
@@ -61,6 +64,8 @@ function init() {
         }
         localStorage.setItem(mcStore, val);
         index = 0;
+        $("#cbThumbs").prop('checked', false);
+        $("#thumbs").empty();
         preload();
         showImage();
     });
@@ -75,13 +80,25 @@ function init() {
 }
 
 function preload() {
+    map = new Map();
+
     for (var idx in images) {
-        $('<img>').attr('src', images[idx].image);
+        map.set(images[idx].image, idx);
+        $('<img>').attr('src', images[idx].image).on('load', function (evt) {
+            var idxImg = map.get(this.src);
+            images[idxImg].width = evt.target.width;
+            images[idxImg].height = evt.target.height;
+        });
     }
 }
 
 function reload() {
     location.reload();
+}
+
+function resize() {
+    setImageSize();
+    showThumbs();
 }
 
 function setImageSize() {
@@ -174,8 +191,55 @@ function randomize() {
     for (let i = images.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [images[i], images[j]] = [images[j], images[i]];
+        map.set(images[i].image, i);
+        map.set(images[j].image, j);
     }
 
     index = 0;
     showImage();
+    showThumbs();
+}
+
+function showThumbs() {
+    var $thumbs = $("#thumbs");
+    var $row = $('<div class="row"></div>');
+    var hasElems = false;
+    var rowWidth = 0;
+    var winWidth = $(window).width();
+
+    $thumbs.empty();
+
+    if (!$("#cbThumbs").is(':checked')) {
+        return;
+    }
+
+    for (var i = 0; i < images.length; i++) {
+        var width = 5 + Math.round(images[i].width * 100 / images[i].height);
+        rowWidth += width;
+        if (rowWidth > winWidth) {
+            $thumbs.append($row);
+            $row = $('<div class="row"></div>');
+            hasElems = false;
+            rowWidth = width;
+        }
+        var $thumb = $("<img class='thumb'>");
+        loadThumb($thumb, images[i].image);
+        $row.append($thumb);
+        hasElems = true;
+    }
+
+    if (hasElems) {
+        $thumbs.append($row);
+    }
+}
+
+function loadThumb($thumb, image) {
+    setTimeout(function() {
+        $thumb.attr("src", image);
+        $thumb.on('click', function (evt) {
+            index = Number(map.get(this.src));
+            $('html, body').animate({ scrollTop: 0 }, 'slow');
+            showImage();
+        });
+    }, 20);
 }
